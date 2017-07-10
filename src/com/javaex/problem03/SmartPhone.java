d767064d4a0c00f2657f813f47acc50ba26ad797
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-10 18:02:11 +0900

    연습문제 3번

--- src/com/javaex/problem03/SmartPhone.java
+++ src/com/javaex/problem03/SmartPhone.java
@@ -4,14 +4,27 @@ public class SmartPhone extends MusicPhone {
     
     public void execute(String str){
         
-        //코드작성
-        
+    	 if("앱".equals(str)){
+             startapp();
+         }else if("음악".equals(str)){
+             super.execute(str);
+         }else {
+        	 System.out.println("통화기능시작");
+         }
+         
+     }
+    public void startapp() {
+    	System.out.println("앱 실행 ");
     }
- 
-    //메소드작성
-    
-    //메소드작성
-    
-    
-    
+        
+    protected void playMusic(){
+        System.out.println("다운로드해서 음악재생");
+    }
 }
+    //메소드작성
+    
+    //메소드작성
+    
+    
+    
+
