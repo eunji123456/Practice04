1d52b2f54e6f61f76e3b8d5a1784fd1eab167a0d
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-10 17:39:34 +0900

    예제1번

--- src/com/javaex/problem01/MyBase.java
+++ src/com/javaex/problem01/MyBase.java
@@ -2,6 +2,21 @@ package com.javaex.problem01;
 
 public class MyBase extends Base{
 
-    //코드작성
-    
-}
+	public void service(String state) {
+		if(state.equals("낮")) {
+	            day();
+	        } else if(state.equals("밤")) {
+	            night();
+	        } else {
+	        	System.out.println("오후에도 낮과 마찬가지로 일해야 합니다.");
+	        }
+	    }
+
+	    public void day() {
+	        System.out.println("낮에는 열씸히 일하자");
+	    }
+
+	    public void night() {
+	        System.out.println("night");
+	    }
+	}
